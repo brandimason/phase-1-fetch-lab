7e1ce3cde399bc1d4a944a3a7789946f44718e93
Brandi L Mason <masonbrandi66@gmail.com>
2023-06-20 15:00:49 -0600

    updated code

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+document.addEventListener('DOMContentLoaded', function(){
+ console.log("loaded")
+});
+
+//fetchBooks() function is called when index.html is loaded
+//inside the function is a fetch request
+function fetchBooks(renderBooks) {
   fetch("https://anapioficeandfire.com/api/books")
   .then(resp => resp.json())
+  // the return response should be converted to JSON
   .then(json => console.log(json));
-  
-}
 
+  return fetch();
+   // To pass the tests, don't forget to return your fetch!
+}
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
@@ -14,7 +21,3 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
